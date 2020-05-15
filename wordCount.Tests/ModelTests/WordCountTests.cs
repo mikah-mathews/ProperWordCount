c060a334ecfec9334ebcbb810713546f4b9e4f22
Mikah Mathews <mikah@outofband.org>
2020-05-15 09:39:30 -0700

    Add first test

--- wordCount.Tests/ModelTests/WordCountTests.cs
+++ wordCount.Tests/ModelTests/WordCountTests.cs
@@ -3,3 +3,21 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCount.Models;
 
+namespace WordCount.TestTools
+{
+  [TestClass]
+  public class WordCountTests
+  {
+    [TestMethod]
+    public void Word_WordToCharacters_CharacterArray()
+    {
+      //Arrange
+      Word wordToCheck = new Word ("cat");
+      char[] target = {'c', 'a', 't'};     
+      //Act
+      char[] result = myWord.WordToCharacters();
+      //Assert
+      CollectionAssert.AreEqual(result, target);
+    }
+  }
+}
